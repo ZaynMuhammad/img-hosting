2e65cb692aa214e8f5bc38a09f1e1a66608af844
zaynmuhammad <zaynmuhammad95@gmail.com>
2024-11-10 18:20:22 -0500

    Add tailwind

--- client/src/components/Home/Home.tsx
+++ client/src/components/Home/Home.tsx
@@ -1,14 +1,15 @@
-
 export function Home() {
 
     return (
-        <>  
-            <nav>
-                <li>My Images</li>
-                <li>Upload</li>
-                <li>Login/signup</li>
-            </nav>
-            <h1>My Homepage</h1>
+        <> 
+            <div className="bg-blue-501 text-black p-4 m-4 rounded-lg">
+                <nav>
+                    <li>My Images</li>
+                    <li>Upload</li>
+                    <li>Login/signup</li>
+                </nav>
+                <h1>My Homepage</h1>
+            </div>
         </>
     )
 }
